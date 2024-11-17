308f846c05b1acc5544b5be5fabf32d7461be16f
Sherwin-14 <varghesesheriwn@gmail.com>
2024-11-17 16:41:47 +0530

    Started modelling to find out best set of parameters for the model

--- pages/sarima.py
+++ pages/sarima.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np 
 import streamlit as st 
 import plotly.graph_objects as go 
-import SARIMAX 
 import pickle
 
 from app import *
@@ -21,15 +20,32 @@ with tab1:
     uplodaded_data = st.file_uploader("Choose a CSV file", type=["csv"])
 
     if uplodaded_data is not None:
+
         df = pd.read_csv(uplodaded_data, parse_dates=['Date']) 
         st.subheader("Data after preprocessing and stationarity check") 
         st.dataframe(df.sample(5), use_container_width=True) 
+
         def run_sarima(df):
+             
              if st.button('Get the best model parameters'): 
                 df['Date'] = pd.to_datetime(df['Date']) 
                 df = df.dropna() 
                 train_df, test_df = train_test_split(df, test_size=0.2, shuffle=False) 
                 model = auto_arima(train_df['Close'], start_p=1, start_d=1, start_q=1, max_p=4, max_d=3, max_q=4, m=12, seasonal=True, error_action='warn', suppress_warnings=True, stepwise=True) 
-                # Print the summary of the best model 
-                st.markdown("#### Best Model Summary:") 
-                st.text(model.summary())
+
+                p, d, q = model.order 
+                P, D, Q, s = model.seasonal_order
+
+                if p == 0 and q == 0 and d == 0: 
+                    st.markdown(""" #### Model order is (0,0,0). This may indicate that the data is a simple random walk. Try to collect more data or use a different model selection method. """) 
+                    return (p, d, q, P, D, Q, s)
+                
+                st.markdown(f""" #### Result : The best SARIMA model for this dataset would have parameters of ({model.order[0]}, {model.order[1]}, {model.order[2]}) and seasonal order of ({model.seasonal_order[0]}, {model.seasonal_order[1]}, {model.seasonal_order[2]}, {model.seasonal_order[3]}) """) 
+                st.session_state.pdq_values = (p, d, q, P, D, Q, s) 
+                return (p, d, q, P, D, Q, s)
+             
+             else: 
+                return (True, True, True, True, True, True, True)
+             
+        p, d, q, P, D, Q, s = run_sarima(df)
+
