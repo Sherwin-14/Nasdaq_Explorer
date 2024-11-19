210e0bb65cffeccfbdc85ea196a7488e859f2955
Sherwin-14 <varghesesheriwn@gmail.com>
2024-11-19 13:11:06 +0530

    Made intial ml modelling page

--- pages/ml_models.py
+++ pages/ml_models.py
@@ -7,4 +7,8 @@ st.subheader("Choose dataset for forecasting")
 
 uplodaded_data = st.file_uploader("Choose a CSV file", type=["csv"],key = "1")
 
+st.subheader("Choose the algorithm")
+
 model = st.selectbox("Select the ML Model",["Linear Regression","XGBoost","Random Forest","Gradient Boosting"])
+
+st.button("Start Forecasting")
