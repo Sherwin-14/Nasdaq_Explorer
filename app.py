f2aa2a6e407e2fbf45c46a82e07e329f52689260
Sherwin-14 <varghesesheriwn@gmail.com>
2024-12-07 15:38:30 +0530

    Added boiler plate to dl_models.py and fixed the name for fb prophet in tab section

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ pages = {
 
     "Resources": [
         st.Page("pages/arima.py", title="ARIMA Modelling", icon="📊"),
-        st.Page("pages/prophet.py", title="Prophet", icon="🛍️"),
+        st.Page("pages/prophet.py", title="Prophet Modelling", icon="🛍️"),
         st.Page("pages/sarima.py", title="SARIMA Modelling", icon="💸"),
         st.Page("pages/ml_models.py", title="Machine Learning Models", icon="⏰"),
         st.Page("pages/dl_models.py",title="Deep Learning Models",icon="📋"),
---
+++ pages/dl_models.py
@@ -0,0 +1,6 @@
+from app import *
+
+
+st.title("Forecasting with DL Models")
+
+uploaded_file = st.file_uploader("Choose a CSV File",type = "csv",key = "40")
