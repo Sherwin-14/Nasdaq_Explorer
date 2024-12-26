c813cce791ed329e4f610d41f4697b34609fc70d
Sherwin-14 <varghesesheriwn@gmail.com>
2024-12-26 17:27:14 +0530

    lstm structure created

--- pages/dl_models.py
+++ pages/dl_models.py
@@ -1,9 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+
+
 from app import *
 
 
 st.title("Forecasting with DL Models")
 
 uploaded_data = st.file_uploader("Choose a CSV File", type="csv", key="40")
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, n_layers, dropout):
+        super(LSTMModel, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(input_dim, hidden_dim, n_layers, batch_first=True, dropout=dropout)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).to(x.device)
+        c0 = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).to(x.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
 
 @st.cache_resource
 def load_data(uploaded_file):
