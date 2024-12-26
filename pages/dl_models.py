6d1e2480971ef1d1f610125004370ef4b71d1e74
Sherwin-14 <varghesesheriwn@gmail.com>
2024-12-26 16:16:31 +0530

    started with deep learning

--- pages/dl_models.py
+++ pages/dl_models.py
@@ -1,6 +1,29 @@
 from app import *
 
 
-st.titleea("Forecasting with DL Models")
+st.title("Forecasting with DL Models")
 
-uploaded_file = st.file_uploader("Choose a CSV File", type="csv", key="40")
+uploaded_data = st.file_uploader("Choose a CSV File", type="csv", key="40")
+
+
+@st.cache_resource
+def load_data(uploaded_file):
+    data = pd.read_csv(uploaded_file, parse_dates=["Date"])
+    return data
+
+if uploaded_data is not None:
+    #data = load_data(uplodaded_data)
+    st.subheader("Choose the algorithm")
+    model_name = st.selectbox(
+        "Select the ML Model", ["LSTM"]
+    )  # Only show feature engineering settings if XGBoost is selected
+
+
+ #if st.button("Start Forecasting"):
+        #X, y = preprocess_data(data)
+        #predictions, rmse, model = train_and_forecast(X, y, model_name, data)
+        #st.subheader("Feature Importance")
+        #plot_feature_importance(model, X.columns)
+        # st.write(next_7_days_pred)
+        # forecast_df = pd.DataFrame({'Date': test_index, 'Actual': y_test, 'Forecast': predictions})
+        # st.write(forecast_df)
