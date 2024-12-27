c64a721a20e28895fd4240ee21a79c8f1e1955c9
Sherwin-14 <varghesesheriwn@gmail.com>
2024-12-27 16:08:51 +0530

    changed ratio of splitting

--- pages/dl_models.py
+++ pages/dl_models.py
@@ -42,7 +42,7 @@ def create_dataset(dataset, time_step=1):
 def preprocess_data(df, time_step): # Scale the data 
     scaler = MinMaxScaler(feature_range=(0, 1))
     df1 = scaler.fit_transform(np.array(df).reshape(-1, 1)) # Splitting dataset into train and test 
-    training_size = int(len(df1) * 0.65) 
+    training_size = int(len(df1) * 0.80) 
     test_size = len(df1) - training_size 
     train_data, test_data = df1[0:training_size, :], df1[training_size:len(df1), :1] # Create dataset for LSTM 
     X_train, y_train = create_dataset(train_data, time_step) 
@@ -50,7 +50,7 @@ def preprocess_data(df, time_step): # Scale the data
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1) 
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1) 
     
-    return X_train, y_train, X_test, ytest
+    return X_train, y_train, X_test, ytest, scaler
 
 
 st.title("Forecasting with DL Models")
@@ -66,30 +66,30 @@ if uploaded_data is not None:
     )  # Only show feature engineering settings if XGBoost is selected
 
 
-if st.button("Start Forecasting"):
-    n = 100 
-    df = load_data(uploaded_data)
-    df1 = df.reset_index()['Close']
-    X_train, y_train, X_test, ytest, scaler = preprocess_data(df1, n)
+    if st.button("Start Forecasting"):
+        n = 100 
+        df = load_data(uploaded_data)
+        df1 = df.reset_index()['Close']
+        X_train, y_train, X_test, ytest, scaler = preprocess_data(df1, n)
 
-    st.write("LSTM model data preprocessing complete.") 
-    st.write("X_train shape:", X_train.shape) 
-    st.write("y_train shape:", y_train.shape) 
-    st.write("X_test shape:", X_test.shape) 
-    st.write("ytest shape:", ytest.shape)
+        st.write("LSTM model data preprocessing complete.") 
+        st.write("X_train shape:", X_train.shape) 
+        st.write("y_train shape:", y_train.shape) 
+        st.write("X_test shape:", X_test.shape) 
+        st.write("ytest shape:", ytest.shape)
 
-    input_dim = 1 
-    hidden_dim = 50 
-    output_dim = 1 
-    n_layers = 2 
-    dropout = 0.2 
-    model = LSTMModel(input_dim, hidden_dim, output_dim, n_layers, dropout) 
-    
-    st.write("LSTM model created.") 
-    st.write(model)
-        #predictions, rmse, model = train_and_forecast(X, y, model_name, data)
-        #st.subheader("Feature Importance")
-        #plot_feature_importance(model, X.columns)
-        # st.write(next_7_days_pred)
-        # forecast_df = pd.DataFrame({'Date': test_index, 'Actual': y_test, 'Forecast': predictions})
+        input_dim = 1 
+        hidden_dim = 50 
+        output_dim = 1 
+        n_layers = 2 
+        dropout = 0.2 
+        model = LSTMModel(input_dim, hidden_dim, output_dim, n_layers, dropout) 
+        
+        st.write("LSTM model created.") 
+        st.write(model)
+            #predictions, rmse, model = train_and_forecast(X, y, model_name, data)
+            #st.subheader("Feature Importance")
+            #plot_feature_importance(model, X.columns)
+            # st.write(next_7_days_pred)
+            # forecast_df = pd.DataFrame({'Date': test_index, 'Actual': y_test, 'Forecast': predictions})
         # st.write(forecast_df)
