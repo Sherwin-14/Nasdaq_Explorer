8d9f47610bb18f1d995e8232c6a62071dcd72686
Sherwin-14 <varghesesheriwn@gmail.com>
2024-12-27 16:03:14 +0530

    added some helper fucntions for lstm

--- pages/dl_models.py
+++ pages/dl_models.py
@@ -4,8 +4,9 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 
-
 from app import *
+from sklearn.preprocessing import MinMaxScaler
+
 
 st.title("Forecasting with DL Models")
 
@@ -33,6 +34,29 @@ def load_data(uploaded_file):
     data = pd.read_csv(uploaded_file, parse_dates=["Date"])
     return data
 
+@st.cache_resource
+def create_dataset(dataset, time_step=1): 
+    dataX, dataY = [], [] 
+    for i in range(len(dataset) - time_step - 1): 
+        a = dataset[i:(i + time_step), 0] 
+        dataX.append(a) 
+        dataY.append(dataset[i + time_step, 0]) 
+
+    return np.array(dataX), np.array(dataY)
+
+def preprocess_data(df, time_step): # Scale the data 
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    df1 = scaler.fit_transform(np.array(df).reshape(-1, 1)) # Splitting dataset into train and test 
+    training_size = int(len(df1) * 0.65) 
+    test_size = len(df1) - training_size 
+    train_data, test_data = df1[0:training_size, :], df1[training_size:len(df1), :1] # Create dataset for LSTM 
+    X_train, y_train = create_dataset(train_data, time_step) 
+    X_test, ytest = create_dataset(test_data, time_step) # Reshape input to be [samples, time steps, features] which is required for LSTM 
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1) 
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1) 
+    
+    return X_train, y_train, X_test, ytest
+
 
 if uploaded_data is not None:
     #data = load_data(uplodaded_data)
@@ -42,7 +66,7 @@ if uploaded_data is not None:
     )  # Only show feature engineering settings if XGBoost is selected
 
 
- #if st.button("Start Forecasting"):
+#if st.button("Start Forecasting"):
         #X, y = preprocess_data(data)
         #predictions, rmse, model = train_and_forecast(X, y, model_name, data)
         #st.subheader("Feature Importance")
